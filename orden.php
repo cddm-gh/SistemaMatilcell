dd7ffe3c0d555e0dce3d375e6a6d3f5df2ada06f
Carlos Delgado <carlosd.dev@gmail.com>
2016-04-28 22:52:03 -0430

    Acomodado el formulario de creacion de ordenes, mas ordenado

--- orden.php
+++ orden.php
@@ -28,44 +28,49 @@ if(!isset($_SESSION['usuario'])){
 			</div>
 		</div>
 		<div class="row">			
-			<div class="col-md-4 col-md-offset-2">
-				<form action="validar_orden.php" method="POST" name="orden">
-					<div class="form-group">
-						<label for="cedula">Cedula:</label>
-						<input type="text" class="form-control" id="cedula" name="cedula" placeholder="Numero De Cedula" required="true" 
-							autofocus="true" maxlength="9">
-					</div>
-					
-					<div class="form-group">
-						<label for="nombre">Nombre:</label>
-						<input type="text" class="form-control textoMayuscula" id="nombre" name="nombre" placeholder="Nombre del cliente" 
-							required="true" maxlength="45">
-					</div>
-					
-					<div class="form-group">
-						<label for="telefono">Telefono:</label>
-						<input type="text" class="form-control "id="telefono" name="telefono" placeholder="Numero De telefono" 
-							required="true" maxlength="12">
-					</div>
-					<hr>
-					<div class="form-group">
-						<label for="serial">Serial:</label>
-						<input type="text" class="form-control "id="serial" name="serial" placeholder="Serial Del equipo" 
-							required="true" maxlength="15">
-					</div>	
-					
-					<div class="form-group">
-						<label for="serial">Marca:</label>
-						<input type="text" class="form-control textoMayuscula" id="marca" name="marca" placeholder="Marca del equipo" 
-							required="true" maxlength="20">
-					</div>
 
-					<div class="form-group">
-						<label for="serial">Modelo:</label>
-						<input type="text" class="form-control textoMayuscula" id="modelo" name="modelo" placeholder="Modelo del equipo" 
-							required="true" maxlength="25">
+				<form action="validar_orden.php" method="POST" name="orden">
+					<div class="col-md-3">
+						<div class="form-group">
+							<label for="cedula">Cedula:</label>
+							<input type="text" class="form-control" id="cedula" name="cedula" placeholder="Numero De Cedula" required="true"
+								autofocus="true" maxlength="9">
+						</div>
+
+						<div class="form-group">
+							<label for="nombre">Nombre:</label>
+							<input type="text" class="form-control textoMayuscula" id="nombre" name="nombre" placeholder="Nombre del cliente"
+								required="true" maxlength="45">
+						</div>
+
+						<div class="form-group">
+							<label for="telefono">Telefono:</label>
+							<input type="text" class="form-control "id="telefono" name="telefono" placeholder="Numero De telefono"
+								required="true" maxlength="12">
+						</div>
+						<hr>
 					</div>
-					<hr>
+					<div class="col-md-3">
+						<div class="form-group">
+							<label for="serial">Serial:</label>
+							<input type="text" class="form-control "id="serial" name="serial" placeholder="Serial Del equipo"
+								required="true" maxlength="15">
+						</div>
+
+						<div class="form-group">
+							<label for="serial">Marca:</label>
+							<input type="text" class="form-control textoMayuscula" id="marca" name="marca" placeholder="Marca del equipo"
+								required="true" maxlength="20">
+						</div>
+
+						<div class="form-group">
+							<label for="serial">Modelo:</label>
+							<input type="text" class="form-control textoMayuscula" id="modelo" name="modelo" placeholder="Modelo del equipo"
+								required="true" maxlength="25">
+						</div>
+						<hr>
+					</div>
+					<div class="col-md-1">
 						<div class="form-group">
 							<label>Memoria SD:</label><br>
 							<input type="radio" id="memorias" name="memoria" value="si">SI
@@ -81,30 +86,49 @@ if(!isset($_SESSION['usuario'])){
 							<input type="radio" id="tapas" name="tapa" value="si" checked="true">SI
 							<input type="radio" id="tapan" name="tapa" value="no">NO
 						</div>
-					
-					<hr>
-					<div class="form-group">
-					<label>Falla(s)</label><br>
-					<textarea name="falla" class="form-control textoMayuscula" id="falla" name="falla" rows="5" cols="25" required wrap="soft" maxlength="50"
-						maxlength="50"></textarea>
-					<br>
-					<select name="tecnicos" class="form-control" id="tecnicos">
-						<option value="nada" selected="true" disabled="true">-- Tecnicos</option>
-						<?php
-							while($row = $statement->fetch()){
-								echo "<option value='".$row['id_tec']."'>" . $row['nombre'] . "</option>";
-							}
-						?>
-					</select>
-
-					<label>Observacion(es)</label><br>
-					<textarea name="observacion" class="form-control textoMayuscula" id="observacion" name="observacion" rows="5" cols="25" 
-						wrap="soft" maxlength="80" maxlength="80"></textarea>
+						<hr>
 					</div>
-					<hr>
-					<input type="submit" class="btn btn-success btn-lg" name="crear" value="Crear Orden">
-					<input type="reset" class="btn btn-primary btn-lg" value="Limpiar Formulario"><br>
-	
+					<div class="col-md-3">
+						<div class="form-group">
+						<label>Falla(s)</label><br>
+						<textarea name="falla" class="form-control textoMayuscula" id="falla" name="falla" rows="5" cols="25" required wrap="soft" maxlength="50"
+							maxlength="50"></textarea>
+						<br>
+						<select name="tecnicos" class="form-control" id="tecnicos">
+							<option value="nada" selected="true" disabled="true">-- Tecnicos</option>
+							<?php
+								while($row = $statement->fetch()){
+									echo "<option value='".$row['id_tec']."'>" . $row['nombre'] . "</option>";
+								}
+							?>
+						</select>
+
+						<label>Observacion(es)</label><br>
+						<textarea name="observacion" class="form-control textoMayuscula" id="observacion" name="observacion" rows="5" cols="25"
+							wrap="soft" maxlength="80" maxlength="80"></textarea>
+						</div>
+						<hr>
+					</div>
+
+					<div class="col-md-2">
+						<label>Total:</label><br>
+						<div class="form-group">
+							<input type="text" class="form-control" placeholder="Cantidad a pagar">
+						</div>
+						<div class="form-group">
+							<input type="text" class="form-control" placeholder="Cantidad abonada">
+						</div>
+						<div class="form-group">
+							<input type="text" class="form-control" placeholder="Cantidad restante">
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-4 col-md-offset-4">
+							<input type="submit" class="btn btn-success btn-lg" name="crear" value="Crear Orden">
+							<input type="reset" class="btn btn-primary btn-lg" value="Limpiar Formulario"><br>
+						</div>
+					</div>
 					<div id="escondido" >
 						<br> Status: <br>
 						<input type="radio" id="srecibido" name="status" value="recibido" checked="true">RECIBIDO <br>
@@ -115,19 +139,7 @@ if(!isset($_SESSION['usuario'])){
 						<input type="radio" id="equipo_enc" name="equipo_enc" value="encontrado">Equipo encontrado <br>
 						<input type="radio" id="cliente_enc" name="equipo_enc" value="nencontrado" checked="true">Equipo NO encontrado <br>
 					</div>
-				</form>	
-			</div>
-			<!-- TODO finalizar los inputs para precio y pago -->
-			<div class="col-md-4">
-				<form action="" name="pago">
-					<div class="form-group">
-						<input type="text" class="form-control" placeholder="Cantidad a pagar">
-					</div>
-					<div class="form-group">
-						<input type="text" class="form-control" placeholder="Cantidad restante">
-					</div>
 				</form>
-			</div>
 		</div>
 	</div>
 	
