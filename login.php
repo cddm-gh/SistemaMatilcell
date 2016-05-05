fee41259bb4637b8ef4f6931e78d5701389143a7
gorydev <carlosd.dev@gmail.com>
2016-05-04 22:44:17 -0430

    cambios login

--- login.php
+++ login.php
@@ -28,7 +28,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 		$_SESSION['usuario'] = $usuario;
 		header('Location: index.php');
 	}else{
-		$errores .= '<li>Datos incorrectossssssss.</li>';
+		$errores .= '<li>Datos incorrectos.</li>';
 	}
 }
 
